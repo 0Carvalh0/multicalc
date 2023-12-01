24ee1a17c81262ec00624b94f9ab0acbc7aef7da
0Carvalh0 <joctor.carvalho2004@gmail.com>
2023-12-01 17:11:08 -0300

    resolvi os problemas do calculo de imc

--- docs/script/imc.js
+++ docs/script/imc.js
@@ -1,16 +1,29 @@
-var n1 = document.getElementById('num1')
-var res = document.getElementById('res')
+var res = document.querySelector('div.res')
 
 function imc() {
   var peso = Number(document.querySelector('input#peso').value)
   var altura = Number(document.querySelector('input#altura').value)
   var imcres = peso / (altura ** 2)
   
-  res.innerHTML = `${imcres}`
-  if (imcres < 17) {
-    res.innerHTML += 'Cuidado! Você está MUITO ABAIXO do peso.'
-  } else if (imcres >= 17 && imcres < 18,5) {
-    res.innerHTML += 'Atenção! Você está ABAIXO do peso.'
+  if (peso == 0 || altura == 0) {
+    window.alert("[ERRO] Insira um valor nos campos!")
+  } else {
+    res.innerHTML = `${imcres.toFixed(2)}<br>`
+    if (imcres < 17) {
+      res.innerHTML += 'Cuidado! Você está MUITO ABAIXO do peso.'
+    } else if (imcres >= 17 && imcres < 18.5) {
+      res.innerHTML += 'Atenção! Você está ABAIXO do peso.'
+    } else if (imcres >= 18.5 && imcres < 24.9) {
+      res.innerHTML += 'Parabéns! Você está no peso IDEAL.'
+    } else if (imcres >= 25 && imcres < 29.9) {
+      res.innerHTML += 'Atenção! Você está ACIMA do peso.'
+    } else if (imcres >= 30 && imcres < 34.9) {
+      res.innerHTML += 'Cuidado! Você está com OBESIDADE nível 1'
+    } else if (imcres >= 35 && imcres < 39.9) {
+      res.innerHTML += 'Urgente! Você está com OBESIDADE II (SEVERA)'
+    } else {
+      res.innerHTML += 'Você está com OBESIDADE III (MÓRBIDA), procure ajuda imediatamente!'
+    }
   }
 }
 
