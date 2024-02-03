0016fb3a8c50fa1ba079f1fc2284373b00cf55da
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-02-03 00:11:32 -0300

    Aprimoramento da calculadora

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,28 @@
-let input = document.querySelector('input#calc__input')
+const res = document.getElementById("calc__visor");
 
-function addValor() {
-    let tecla = document.querySelector('div.tecla').value
-    input.innerHTML += '5'
+function addValor(num) {
+  let texto = document.querySelector("p#calc__visor").innerHTML;
+  if (texto === "[ERRO]...") {
+    res.innerHTML = num;
+  } else {
+    res.innerHTML += num;
+  }
+}
+
+function limpar() {
+  res.innerHTML = "";
+}
+
+function deletar() {
+  let texto = document.querySelector("p#calc__visor").innerHTML;
+  res.innerHTML = texto.substring(0, texto.length - 1);
+}
+
+function calcular() {
+  let texto = document.querySelector("p#calc__visor").innerHTML;
+  if (texto) {
+    res.innerHTML = eval(texto);
+  } else {
+    res.innerHTML = "[ERRO]...";
+  }
 }
