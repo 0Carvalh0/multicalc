73c2fd3921aa68850acabd2b3a2dd2462cccc63b
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-02-11 18:38:03 -0300

    Sidebar fix

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -2,15 +2,15 @@ const btnMenu = document.getElementById("btn__menuhamburguer");
 const listaMenu = document.getElementById("lista__menu");
 
 btnMenu.addEventListener("click", () => {
-  if (listaMenu.style.marginLeft < "0") {
-    listaMenu.style.marginLeft = "0";
+  if (listaMenu.style.left < "0") {
+    listaMenu.style.left = "0";
     btnMenu.style.backgroundColor = "#fff";
     btnMenu.style.color = "var(--cor5)";
 
     btnMenu.classList.remove("fa-bars");
     btnMenu.classList.add("fa-xmark");
   } else {
-    listaMenu.style.marginLeft = "-200px";
+    listaMenu.style.left = "-210px";
     btnMenu.style.backgroundColor = "var(--cor5)";
     btnMenu.style.color = "#fff";
 
