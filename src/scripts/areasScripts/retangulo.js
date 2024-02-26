67e2ea1e88433545f5f88625b62ca898b23794df
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-02-26 15:09:40 -0300

    finalizei o calculo de retangulo

---
+++ src/scripts/areasScripts/retangulo.js
@@ -0,0 +1,10 @@
+const base = document.querySelector("input#iform__base");
+const altura = document.querySelector("input#iform__altura");
+const res = document.querySelector("div#res");
+
+function retangulo() {
+  let area = base.value * altura.value;
+
+  res.style.opacity = 1;
+  res.innerHTML = area + "m²";
+}
