defe4aacfb6b7f2b60a6edea41e1245c2ea2d817
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-04-16 16:07:27 -0300

    feat: calculadora para mobile finalizada

--- src/scripts/calc.js
+++ src/scripts/calc.js
@@ -1,7 +1,9 @@
-const res = document.getElementById("calc__visor-atual");
+const res = document.querySelector(".main-container__display-current");
 
-function addValor(num) {
-  let texto = document.querySelector("p#calc__visor-atual").innerHTML;
+function addValue(num) {
+  let texto = document.querySelector(
+    ".main-container__display-current"
+  ).innerHTML;
 
   if (texto === "") {
     let regex = /[\+\-\*\/\.]$/;
@@ -30,22 +32,23 @@ function addValor(num) {
   }
 }
 
-function limpar() {
+function clear() {
   res.innerHTML = "";
-  document.getElementById("calc__visor-anterior").innerHTML = "";
+  document.querySelector(".main-container__display-prev").innerHTML = "";
 }
 
-function deletar() {
-  let texto = document.querySelector("p#calc__visor-atual").innerHTML;
+function del() {
+  let texto = document.querySelector(".main-container__display-current").innerHTML;
   res.innerHTML = texto.substring(0, texto.length - 1);
 }
 
-function calcular() {
-  let texto = document.querySelector("p#calc__visor-atual").innerHTML;
+function calc() {
+  let texto = document.querySelector(
+    ".main-container__display-current"
+  ).innerHTML;
   if (texto) {
-    document.getElementById("calc__visor-anterior").innerHTML = texto;
+    document.querySelector(".main-container__display-prev").innerHTML =
+      texto;
     res.innerHTML = eval(texto);
-  } else {
-    window.alert("[ERRO] Por favor, insira um número para calcular.");
   }
 }
